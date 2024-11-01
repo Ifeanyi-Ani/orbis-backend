d5f0076106f0efacded499a1b1c4e869cdca8ec1
Ifeanyi Ani <ifeanyiani155@gmail.com>
2024-11-01 18:06:31 +0100

    [authenticate-features] chores:remove unneccessary files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersController } from './users/users.controller';
+import { AuthController } from './auth/auth.controller';
+import { UsersService } from './users/users.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [UsersController, AuthController],
+  providers: [UsersService],
 })
 export class AppModule {}
