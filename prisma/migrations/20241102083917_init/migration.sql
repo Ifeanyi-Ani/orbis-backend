32b583d2cc209805b0465116b1a96b673ec0a0d7
Ifeanyi Ani <ifeanyiani155@gmail.com>
2024-11-02 11:51:04 +0100

    [authenticate-features] alter schema

---
+++ prisma/migrations/20241102083917_init/migration.sql
@@ -0,0 +1,22 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `firstName` on the `Profile` table. All the data in the column will be lost.
+  - You are about to drop the column `lastName` on the `Profile` table. All the data in the column will be lost.
+  - Added the required column `address` to the `Profile` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `bio` to the `Profile` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `country` to the `Profile` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `firstName` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `lastName` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Profile" DROP COLUMN "firstName",
+DROP COLUMN "lastName",
+ADD COLUMN     "address" TEXT NOT NULL,
+ADD COLUMN     "bio" TEXT NOT NULL,
+ADD COLUMN     "country" TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "firstName" TEXT NOT NULL,
+ADD COLUMN     "lastName" TEXT NOT NULL;
