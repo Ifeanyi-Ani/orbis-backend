55ff24c57d7d0b69db2b752a26e475e1ceebda5b
Ifeanyi Ani <ifeanyiani155@gmail.com>
2024-11-25 15:29:41 +0100

    [user-feat] creation of user is been handle in the user service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -6,10 +6,11 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { Prisma } from '@prisma/client';
+import { Prisma, Role } from '@prisma/client';
 import * as bcrypt from 'bcrypt';
 import { Response } from 'express';
 import { DatabaseService } from 'src/database/database.service';
+import { CreateAdminDto } from 'src/users/dto/create-admin.dto';
 
 @Injectable()
 export class AuthService {
@@ -18,29 +19,8 @@ export class AuthService {
     private jwtService: JwtService,
   ) {}
 
-  async register(createUserDto: Partial<Prisma.UserCreateInput>) {
-    const existingUser = await this.DatabaseService.user.findUnique({
-      where: { email: createUserDto.email },
-    });
-
-    if (existingUser) {
-      throw new ConflictException('User already exists');
-    }
-
-    const hashedPassword = await bcrypt.hash(createUserDto.password, 12);
-
-    const user = await this.DatabaseService.user.create({
-      data: {
-        email: createUserDto.email,
-        password: hashedPassword,
-        firstName: createUserDto.firstName,
-        lastName: createUserDto.lastName,
-        role: createUserDto.role,
-        type: createUserDto.type,
-      } as Prisma.UserCreateInput,
-    });
-
-    return user;
+  async register() {
+    throw new ConflictException('use the right user route for creating user');
   }
 
   async login(email: string, password: string, @Res() res: Response) {
@@ -64,7 +44,6 @@ export class AuthService {
     }
 
     const { access_token, refresh_token } = this.generateToken(user);
-    console.log('we are here');
 
     res.cookie('access_token', access_token, {
       httpOnly: true,
@@ -80,7 +59,7 @@ export class AuthService {
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
-    res.status(200).json({ access_token, refresh_token });
+    res.status(200).json({ access_token });
   }
 
   private generateToken(user: Prisma.UserCreateInput) {
@@ -139,7 +118,6 @@ export class AuthService {
 
     return res.json({
       access_token: newAccessToken,
-      refresh_token: newRefreshToken,
     });
   }
 }
