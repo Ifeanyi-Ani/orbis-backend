064d4b4bcef755d1d619fb31be82a807df5a233c
Ifeanyi Ani <ifeanyiani155@gmail.com>
2024-11-25 15:31:40 +0100

    [user-feat] remove unnecessary code in the user service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,23 +3,10 @@ import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: Prisma.UserCreateInput) {
-    return 'This action adds a new user';
+
+
+
   }
 
-  findAll() {
-    return `This action returns all users`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: Prisma.UserUpdateInput) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
   }
 }
