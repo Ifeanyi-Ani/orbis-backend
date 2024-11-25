ee5bcf1c3d19cd01e53e2bc959f072989564d754
Ifeanyi Ani <ifeanyiani155@gmail.com>
2024-11-25 16:53:22 +0100

    [user-feat] return exeption not an admin

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -5,7 +5,15 @@ import { Prisma } from '@prisma/client';
 export class UsersService {
   constructor(private readonly database: DatabaseService) {}
 
+  async createAdmin(createAdminDto: Partial<CreateAdminDto>) {
+    const { role, firstName, lastName, password, email, ...additionalDetails } =
+      createAdminDto;
 
+    if (!(role === Role.ADMIN)) {
+      throw new BadRequestException(
+        'Please use the right route for creating the user',
+      );
+    }
 
   }
 
