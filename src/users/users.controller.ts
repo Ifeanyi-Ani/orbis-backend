f441fe71b99edeb1868f76e75fee0122a9f49ddc
Ifeanyi Ani <ifeanyiani155@gmail.com>
2024-11-25 16:59:55 +0100

    [user-feat] create patient route

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, HttpCode, Post, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreatePatientDto } from './dto/create-patient.dto';
 import { Protect } from 'src/auth/guard/protect.guard';
 import { RolesGuard } from 'src/auth/guard/roles.guard';
 import { Role } from '@prisma/client';
@@ -11,6 +12,11 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @UseGuards(Protect, RolesGuard)
+  @Roles(Role.RECEPTIONIST, Role.ADMIN)
+  @HttpCode(201)
+  @Post('patients')
+  createPatient(@Body() createPatientDto: CreatePatientDto) {
+    return this.usersService.createPatient(createPatientDto);
   }
 
   @Post('admin')
