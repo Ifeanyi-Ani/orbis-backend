356d12fa21ed2eda93e5b097760bf01e9bf8d218
Ifeanyi Ani <ifeanyiani155@gmail.com>
2024-11-25 17:00:37 +0100

    [user-feat] enable cors
    still need to configure it to fit my use case

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import helmet from 'helmet';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
+  app.enableCors();
   app.use(cookieParser());
   await app.listen(3001);
 }
