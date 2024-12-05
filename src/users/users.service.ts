b550c292fdee3ea63b0090883d21336ad3c39c9a
Ifeanyi Ani <ifeanyiani155@gmail.com>
2024-12-05 19:37:53 +0100

    [user-feat] sync with new model and better error catching

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,89 +1,145 @@
-import {
-  BadRequestException,
-  ConflictException,
-  Injectable,
-} from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreatePatientDto } from './dto/create-patient.dto';
 import { DatabaseService } from 'src/database/database.service';
 import { Role } from '@prisma/client';
 import { CreateAdminDto } from './dto/create-admin.dto';
 import * as bcrypt from 'bcrypt';
+import { BaseExceptionFilter } from '@nestjs/core';
+import { PrismaErrorHandler } from 'src/exception/prisma.exception';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly database: DatabaseService) {}
 
-  async createAdmin(createAdminDto: Partial<CreateAdminDto>) {
-    const { role, firstName, lastName, password, email, ...additionalDetails } =
-      createAdminDto;
+  async createAdmin(createAdminDto: CreateAdminDto) {
+    try {
+      const {
+        role,
+        firstName,
+        lastName,
+        password,
+        email,
+        dob,
+        contactNumber,
+        ...additionalDetails
+      } = createAdminDto;
 
-    if (!(role === Role.ADMIN)) {
-      throw new BadRequestException(
-        'Please use the right route for creating the user',
-      );
-    }
+      if (!(role === Role.ADMIN)) {
+        throw new BadRequestException(
+          'Please use the right route for creating the user',
+        );
+      }
 
-    const existingUser = await this.database.user.findUnique({
-      where: { email },
-    });
+      // encrypt password
+      const hashedPassword = await bcrypt.hash(password, 12);
 
-    if (existingUser) {
-      throw new ConflictException('User already exists');
-    }
+      await this.database.$transaction(async (db) => {
+        const user = await db.user.create({
+          data: {
+            email,
+            password: hashedPassword,
+            firstName,
+            lastName,
+            role,
+            dob,
+            contactNumber,
+          },
+        });
 
-    const hashedPassword = await bcrypt.hash(password, 12);
+        const admin = await db.admin.create({
+          data: {
+            userId: user.id,
+            ...additionalDetails,
+          },
+        });
 
-    return this.database.$transaction(async (db) => {
-      const user = await db.user.create({
-        data: {
-          email,
-          password: hashedPassword,
-          firstName,
-          lastName,
-          role,
-        },
+        user.password = undefined;
+
+        return { ...user, admin };
       });
+    } catch (error) {
+      if (error instanceof BaseExceptionFilter) {
+        throw error;
+      }
 
-      const admin = await db.admin.create({
-        data: {
-          ...additionalDetails,
-          userId: user.id,
-        },
-      });
-
-      user.password = undefined;
-
-      return { ...user, admin };
-    });
+      console.log(error?.meta?.target);
+      PrismaErrorHandler.handle(error);
+    }
   }
 
   async createPatient(createPatientDto: CreatePatientDto) {
-    const { medicalHistory, insuranceInfo, dob, ...userData } =
-      createPatientDto;
+    try {
+      const {
+        firstName,
+        lastName,
+        password,
+        email,
+        dob,
+        contactNumber,
+        ...additionalDetails
+      } = createPatientDto;
 
-    const hashedPassword = await bcrypt.hash(userData.password, 12);
+      const hashedPassword = await bcrypt.hash(password, 12);
 
-    return this.database.$transaction(async (db) => {
-      const user = await db.user.create({
-        data: {
-          ...userData,
-          password: hashedPassword,
-          role: Role.PATIENT,
-        },
+      await this.database.$transaction(async (db) => {
+        const user = await db.user.create({
+          data: {
+            firstName,
+            lastName,
+            email,
+            contactNumber,
+            dob,
+            password: hashedPassword,
+            role: Role.PATIENT,
+          },
+        });
+
+        const patient = await db.patient.create({
+          data: {
+            userId: user.id,
+            ...additionalDetails,
+          },
+        });
+
+        user.password = undefined;
+
+        return { ...user, patient };
       });
+    } catch (error) {
+      if (error instanceof BaseExceptionFilter) {
+        throw error;
+      }
+      console.log({ error });
+      PrismaErrorHandler.handle(error);
+    }
+  }
 
-      const patient = await db.patient.create({
-        data: {
-          userId: user.id,
-          dob,
-          medicalHistory,
-          insuranceInfo,
-        },
+  async findAllPatients(includeDetails = true) {
+    try {
+      const patients = await this.database.user.findMany({
+        where: { role: Role.PATIENT },
+        include: { patient: includeDetails },
       });
+      return { patients };
+    } catch (error) {
+      if (error instanceof BaseExceptionFilter) {
+        throw error;
+      }
+      PrismaErrorHandler.handle(error);
+    }
+  }
 
-      user.password = undefined;
-
-      return { ...user, patient };
-    });
+  async findOnePatients(id: string) {
+    try {
+      return this.database.user.findUnique({
+        where: { id },
+        include: { patient: true },
+      });
+    } catch (error) {
+      if (error instanceof BaseExceptionFilter) {
+        throw error;
+      }
+      PrismaErrorHandler.handle(error);
+    }
   }
 }
