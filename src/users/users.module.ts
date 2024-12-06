c840da421cf0127e5a5c26ffbc0d3d834a3a714e
Ifeanyi Ani <ifeanyiani155@gmail.com>
2024-12-06 23:49:20 +0100

    [user-feat] sync

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { AuthModule } from 'src/auth/auth.module';
+import { TokenModule } from 'src/tokens/tokens.module';
+import { MailModule } from 'src/mail/mail.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, TokenModule, MailModule],
   controllers: [UsersController],
   providers: [UsersService],
 })
